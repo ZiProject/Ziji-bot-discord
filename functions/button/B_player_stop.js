710c3ceffb4b70b484dfcb3e1367dd521c11df76
Ziji <Zijipia@gmail.com>
2025-09-20 23:21:44 +0700

    Update B_player_stop.js

--- functions/button/B_player_stop.js
+++ functions/button/B_player_stop.js
@@ -1,23 +1,22 @@
+const { getPlayer } = require("ziplayer");
+
 module.exports.data = {
 	name: "B_player_stop",
 	type: "button",
-	category: "musix",
+	category: "musix-s",
 	lock: true,
-	ckeckVoice: true,
 };
 
 /**
  * @param { object } button - object button
  * @param { import ("discord.js").ButtonInteraction } button.interaction - button interaction
  * @param { import('../../lang/vi.js') } button.lang - language
- * @param {import("ziplayer").Player} button.player - player
  * @returns
  */
 
-module.exports.execute = async ({ interaction, lang, player }) => {
+module.exports.execute = async ({ interaction, lang }) => {
 	await interaction.deferUpdate().catch(() => {});
-	if (!player?.connection) return interaction.followUp({ content: lang.music.NoPlaying, ephemeral: true });
-
 	interaction.message.edit({ components: [] }).catch((e) => {});
-	player.destroy();
+	const player = getPlayer(interaction.guild.id);
+	player.destroy?.().catch((e) => {});
 };
