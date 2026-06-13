9f8be2748ef26b6fd28880018c356c92b01b63b0
Ziji <104454302+zijipia@users.noreply.github.com>
2026-06-13 22:06:38 +0900

    Update ZiRank.js

--- functions/ranksys/ZiRank.js
+++ functions/ranksys/ZiRank.js
@@ -1,5 +1,17 @@
 const { useHooks } = require("zihooks");
 const config = useHooks.get("config");
+const path = require("path");
+
+function getLang(lang) {
+	const defaultLang = config?.DefaultLang || "en";
+	const selectedLang = lang || defaultLang;
+
+	try {
+		return require(path.join(__dirname, "..", "..", "lang", selectedLang));
+	} catch {
+		return require(path.join(__dirname, "..", "..", "lang", defaultLang));
+	}
+}
 
 module.exports.data = {
 	name: "ZiRank",
@@ -8,43 +20,47 @@ module.exports.data = {
 
 module.exports.execute = async ({ user, XpADD = 1, CoinADD = 0 }) => {
 	if (!useHooks) return;
+	try {
+		const DataBase = useHooks.get("db");
+		if (DataBase && user) {
+			const userData = (await DataBase.ZiUser.findOne({ userID: user.id })) || {};
 
-	const DataBase = useHooks.get("db");
-	if (DataBase && user) {
-		const userData = (await DataBase.ZiUser.findOne({ userID: user.id })) || {};
+			const xp = userData.xp ?? 1;
+			const level = userData.level ?? 1;
+			const coin = userData.coin ?? 0;
+			const { lang, color } = userData;
 
-		const xp = userData.xp ?? 1;
-		const level = userData.level ?? 1;
-		const coin = userData.coin ?? 0;
-		const { lang, color } = userData;
+			let newXp = xp + XpADD;
+			let newLevel = level;
+			let newCoin = coin + CoinADD;
 
-		let newXp = xp + XpADD;
-		let newLevel = level;
-		let newCoin = coin + CoinADD;
+			const xpThreshold = newLevel * 50 + 1;
+			if (newXp > xpThreshold) {
+				newLevel += 1;
+				newXp = 1;
+				newCoin += newLevel * 100;
+			}
 
-		const xpThreshold = newLevel * 50 + 1;
-		if (newXp > xpThreshold) {
-			newLevel += 1;
-			newXp = 1;
-			newCoin += newLevel * 100;
-		}
-
-		await DataBase.ZiUser.updateOne(
-			{ userID: user.id },
-			{
-				$set: {
-					xp: newXp,
-					level: newLevel,
-					coin: newCoin,
+			await DataBase.ZiUser.updateOne(
+				{ userID: user.id },
+				{
+					$set: {
+						xp: newXp,
+						level: newLevel,
+						coin: newCoin,
+					},
 				},
-			},
-			{ upsert: true },
-		);
+				{ upsert: true },
+			);
 
-		const langdef = require(`./../../lang/${lang || config?.DefaultLang}`);
-		langdef.color = color;
-		return langdef;
+			const langdef = getLang(lang);
+
+			langdef.color = color;
+			return langdef;
+		}
+	} catch (error) {
+		console.error("Error in ZiRank:", error);
 	}
 
-	return require(`./../../lang/${config?.DefaultLang}`);
+	return getLang(config?.DefaultLang || "en");
 };
